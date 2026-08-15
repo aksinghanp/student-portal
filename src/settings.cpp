3b35f5aac45bd73b3cba5b12c015eada0ba8e1de
aksinghanp <aksinghanp75@gmail.com>
2026-08-15 18:11:05 +0530

    Add interactive settings menu

--- src/settings.cpp
+++ src/settings.cpp
@@ -32,6 +32,12 @@ void updateNotifications(Settings& settings) {
     }
 }
 
+void changeUsername(Settings& settings) {
+    cout << "Enter new username: ";
+    cin.ignore();
+    getline(cin, settings.username);
+}
+
 int main() {
     Settings settings;
 
@@ -40,12 +46,39 @@ int main() {
 
     settings.notifications = true;
 
-    displaySettings(settings);
+    int choice;
 
-    updateNotifications(settings);
+    do {
+        cout << "\n===== SETTINGS MENU =====" << endl;
+        cout << "1. Display settings" << endl;
+        cout << "2. Change username" << endl;
+        cout << "3. Update notifications" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
 
-    cout << "\nUpdated settings:" << endl;
-    displaySettings(settings);
+        switch (choice) {
+            case 1:
+                displaySettings(settings);
+                break;
+
+            case 2:
+                changeUsername(settings);
+                break;
+
+            case 3:
+                updateNotifications(settings);
+                break;
+
+            case 4:
+                cout << "Exiting settings..." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice!" << endl;
+        }
+
+    } while (choice != 4);
 
     return 0;
 }
