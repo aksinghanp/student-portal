5c650827c2eace8125c7f1483f5f13d733055a76
aksinghanp <aksinghanp75@gmail.com>
2026-08-15 16:05:21 +0530

    Add login module and ignore login executable

---
+++ src/login.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool login(const string& username, const string& password) {
+    return username == "student" && password == "1234";
+}
+
+int main() {
+    string username;
+    string password;
+
+    cout << "Username: ";
+    cin >> username;
+
+    cout << "Password: ";
+    cin >> password;
+
+    if (login(username, password)) {
+        cout << "Login successful." << endl;
+    } else {
+        cout << "Invalid username or password." << endl;
+    }
+
+    return 0;
+}
